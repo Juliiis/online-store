b990ab200f7ea68932e46a3ae7eefea175a20ebe
julieta <julieta.martin@clarity.ai>
2022-10-14 17:39:23 +0200

    refactor: extract decideWhatToDoNext into a new class

--- app/src/main/java/gradle/java/infrastructure/CatalogueFormater.java
+++ app/src/main/java/gradle/java/infrastructure/CatalogueFormater.java
@@ -8,8 +8,6 @@ import java.util.Scanner;
 public class CatalogueFormater {
     ProductsToChooseExplore productsToChooseExplore = new ProductsToChooseExplore();
     DecideStepToContinue decideStepToContinue = new DecideStepToContinue();
-    ProductWarehouse productWarehouse = new ProductWarehouse();
-    CatalogueFormater catalogueFormater = new CatalogueFormater();
     public String format(ArrayList<Product> catalogue){
         for (Product product : catalogue){
             System.out.println(product.showImage());
@@ -20,28 +18,9 @@ public class CatalogueFormater {
             System.out.println("\n");
         }
         productsToChooseExplore.productsToChooseExplore();
-        decideWhatToDoNext();
+        decideStepToContinue.decideWhatToDoNext();
         return "";
     }
 
-    public void decideWhatToDoNext(){
-        System.out.println("\n");
-        System.out.println("¿What would you like to do next?");
-        String nextOption1 = "1 - Add product to cart";
-        String nextOption2 = "2 - Keep browsing products";
-        System.out.println(nextOption1);
-        System.out.println(nextOption2);
-
-        Scanner myObj = new Scanner(System.in);
-        String opcionChoosed = myObj.nextLine();
-
-        if(opcionChoosed.equals("1")){
-            System.out.println();
-        } else if (opcionChoosed.equals("2")) {
-            ArrayList<Product> catalogue = productWarehouse.findAll();
-            String formattedCatalogue = catalogueFormater.format(catalogue);
-            System.out.println(formattedCatalogue);
-        } else System.out.println("Sorry, you have to choose one valid option");
-    }
 
 }
--- app/src/main/java/gradle/java/infrastructure/DecideStepToContinue.java
+++ app/src/main/java/gradle/java/infrastructure/DecideStepToContinue.java
@@ -1,8 +1,30 @@
 package gradle.java.infrastructure;
 
+import gradle.java.domain.Product;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class DecideStepToContinue {
+  ProductWarehouse productWarehouse = new ProductWarehouse();
+  CatalogueFormater catalogueFormater = new CatalogueFormater();
+  public void decideWhatToDoNext(){
+    System.out.println("\n");
+    System.out.println("¿What would you like to do next?");
+    String nextOption1 = "1 - Add product to cart";
+    String nextOption2 = "2 - Keep browsing products";
+    System.out.println(nextOption1);
+    System.out.println(nextOption2);
+
+    Scanner myObj = new Scanner(System.in);
+    String opcionChoosed = myObj.nextLine();
+
+    if(opcionChoosed.equals("1")){
+      System.out.println();
+    } else if (opcionChoosed.equals("2")) {
+      ArrayList<Product> catalogue = productWarehouse.findAll();
+      String formattedCatalogue = catalogueFormater.format(catalogue);
+      System.out.println(formattedCatalogue);
+    } else System.out.println("Sorry, you have to choose one valid option");
+  }
 
 }
