cbd9f2927bd2ccfeb99236355ef0f64028108865
julieta <julieta.martin@clarity.ai>
2022-10-14 17:45:01 +0200

    refactor: extract GlobalVariables to another class

---
+++ app/src/main/java/gradle/java/infrastructure/GlobalVariables.java
@@ -0,0 +1,6 @@
+package gradle.java.infrastructure;
+
+public class GlobalVariables {
+  String DESCRIPTION_TV = "DESCRIPTION: With this panoramic television, your friday nights will be boring no more. The screen is composed of 14 million pixels. Thanks to its low energy consumption design, your bills will stay low.";
+  String DESCRIPTION_PIANO = "DESCRIPTION: Tired of your noisy neighbourgh? Play this untuned musical keyboard for two hours at home and your neighbour will be ready to move to a building far away from you.";
+}
