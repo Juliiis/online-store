7d0be3dc6d0f257c8ce1705f521f48cbc0e37566
julieta <julieta.martin@clarity.ai>
2022-10-16 14:19:09 +0200

    refactor: rename variable name for GlobalVariables

--- app/src/main/java/gradle/java/infrastructure/ProductsToChooseExplore.java
+++ app/src/main/java/gradle/java/infrastructure/ProductsToChooseExplore.java
@@ -3,9 +3,9 @@ package gradle.java.infrastructure;
 import java.util.Scanner;
 
 public class ProductsToChooseExplore {
-    GlobalVariables variables = new GlobalVariables();
+    GlobalVariables globalVariables = new GlobalVariables();
     public void productsToChooseExplore(){
-        System.out.println(variables.NEXT_PRODUCT_TO_EXPLORE);
+        System.out.println(globalVariables.NEXT_PRODUCT_TO_EXPLORE);
         String option1 = "1 - \uD83D\uDCFA";
         String option2 = "2 - \uD83C\uDFB9";
         System.out.println(option1);
@@ -15,9 +15,9 @@ public class ProductsToChooseExplore {
         String object = myObj.nextLine();
 
         if(object.equals("1")){
-            System.out.println(variables.DESCRIPTION_TV);
+            System.out.println(globalVariables.DESCRIPTION_TV);
         } else if (object.equals("2")) {
-            System.out.println(variables.DESCRIPTION_PIANO);
-        } else System.out.println(variables.PRODUCT_DOES_NOT_EXIST);
+            System.out.println(globalVariables.DESCRIPTION_PIANO);
+        } else System.out.println(globalVariables.PRODUCT_DOES_NOT_EXIST);
     }
 }
