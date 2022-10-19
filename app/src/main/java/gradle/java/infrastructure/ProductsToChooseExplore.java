26b237895ed903a7b8be09f0f3ac5c007778185d
julieta <julieta.martin@clarity.ai>
2022-10-19 17:45:13 +0200

    refactor: extract global variables into a new class

--- app/src/main/java/gradle/java/infrastructure/ProductsToChooseExplore.java
+++ app/src/main/java/gradle/java/infrastructure/ProductsToChooseExplore.java
@@ -3,9 +3,10 @@ package gradle.java.infrastructure;
 import java.util.Scanner;
 
 public class ProductsToChooseExplore {
-    GlobalVariables globalVariables = new GlobalVariables();
+    String DESCRIPTION_TV = "DESCRIPTION: With this panoramic television, your friday nights will be boring no more. The screen is composed of 14 million pixels. Thanks to its low energy consumption design, your bills will stay low.";
+    String DESCRIPTION_PIANO = "DESCRIPTION: Tired of your noisy neighbourgh? Play this untuned musical keyboard for two hours at home and your neighbour will be ready to move to a building far away from you.";
     public void productsToChooseExplore(){
-        System.out.println(globalVariables.NEXT_PRODUCT_TO_EXPLORE);
+        System.out.println("Which product would you like to explore?");
         String option1 = "1 - \uD83D\uDCFA";
         String option2 = "2 - \uD83C\uDFB9";
         System.out.println(option1);
@@ -15,9 +16,10 @@ public class ProductsToChooseExplore {
         String object = myObj.nextLine();
 
         if(object.equals("1")){
-            System.out.println(globalVariables.DESCRIPTION_TV);
+            System.out.println(DESCRIPTION_TV);
         } else if (object.equals("2")) {
-            System.out.println(globalVariables.DESCRIPTION_PIANO);
-        } else System.out.println(globalVariables.PRODUCT_DOES_NOT_EXIST);
+            System.out.println(DESCRIPTION_PIANO);
+        } else System.out.println("Sorry, the product doesn't exist");
+
     }
 }
