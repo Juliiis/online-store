755d5e47fb67c4ed771acf1307e125ae7c4c7aa8
julieta <julieta.martin@clarity.ai>
2022-10-19 17:58:41 +0200

    refactor: add reference into product

--- app/src/main/java/gradle/java/infrastructure/ProductWarehouse.java
+++ app/src/main/java/gradle/java/infrastructure/ProductWarehouse.java
@@ -11,13 +11,13 @@ public class ProductWarehouse {
                     "Glamorous panoramic television, 13 inches",
                     "With this panoramic television, your friday nights will be boring no more.",
                     300.99,
-                    "W2C"),
+                    "Reference: W2C"),
             new Product(
                     "\uD83C\uDFB9",
                     "Untuned musical keyboard, 4 octaves",
                     "Tired of your noisy neighbourgh? Play this untuned musical keyboard for two hours at home and your neighbour will be ready to move to a building far away from you.",
                     1003.00,
-                    "X4A")));
+                    "Reference: X4A")));
 
     public ArrayList<Product> findAll() {
         return products;
