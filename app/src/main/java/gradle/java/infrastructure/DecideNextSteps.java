7cf75ad6f845c5cd7262c6537e7d5a4c325a47c6
julieta <julieta.martin@clarity.ai>
2022-10-21 23:00:21 +0200

    refactor: modified decideWhatToDoNext method in DecideNextSteps class adding new functionalities

--- app/src/main/java/gradle/java/infrastructure/DecideNextSteps.java
+++ app/src/main/java/gradle/java/infrastructure/DecideNextSteps.java
@@ -1,13 +1,13 @@
 package gradle.java.infrastructure;
 
 import gradle.java.domain.Product;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class DecideNextSteps {
   GlobalVariables globalVariables = new GlobalVariables();
   OnlineShop onlineShop = new OnlineShop();
-
+  ProductWarehouse productWarehouse = new ProductWarehouse();
+  PrintCatalogueFormatter printCatalogueFormatter = new PrintCatalogueFormatter();
   public void decideProductsToExplore(){
     System.out.println(globalVariables.NEXT_PRODUCT_TO_EXPLORE);
     System.out.println(globalVariables.PRODUCT_REFERENCE);
@@ -27,6 +27,15 @@ public class DecideNextSteps {
       System.out.println();
     } else if (opcionChoosed.equals(globalVariables.NUMBER_TWO)) {
       onlineShop.showProducts();
+      decideProductsToExplore();
+
+      Scanner object = new Scanner(System.in);
+      String test = object.nextLine();
+
+      Product product = productWarehouse.findProductByReference(test);
+      printCatalogueFormatter.printByReference(product);
+      decideWhatToDoNext();
+
     } else System.out.println(globalVariables.INVALID_OPTION);
 
   }
--- app/src/main/java/gradle/java/infrastructure/OnlineShop.java
+++ app/src/main/java/gradle/java/infrastructure/OnlineShop.java
@@ -6,15 +6,15 @@ import java.util.Scanner;
 
 public class OnlineShop {
     private final ProductWarehouse productWarehouse = new ProductWarehouse();
-    PrintCatalogueFormatter print = new PrintCatalogueFormatter();
+    PrintCatalogueFormatter printCatalogueFormatter = new PrintCatalogueFormatter();
     public void showProducts() {
         ArrayList<Product> catalogue = productWarehouse.findAll();
-        print.printCatalogue(catalogue);
+        printCatalogueFormatter.printCatalogue(catalogue);
     }
     public void showProductByReference(){
         Scanner myObj = new Scanner(System.in);
         String object = myObj.nextLine();
         Product product = productWarehouse.findProductByReference(object);
-        print.printByReference(product);
+        printCatalogueFormatter.printByReference(product);
     }
 }
