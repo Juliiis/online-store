20b693d349855fa2d2d9e0e5aeaac54b515ae91e
julieta <julieta.martin@clarity.ai>
2022-10-21 17:25:37 +0200

    refactor: modified content of PRODUCT_REFERENCE variable

--- app/src/main/java/gradle/java/infrastructure/GlobalVariables.java
+++ app/src/main/java/gradle/java/infrastructure/GlobalVariables.java
@@ -5,7 +5,7 @@ public class GlobalVariables {
   String NUMBER_TWO = "2";
   String DECIDE_NEXT_STEP = "¿What would you like to do next?";
   String NEXT_PRODUCT_TO_EXPLORE = "Which product would you like to explore?";
-  String PRODUCT_REFERENCE = "Please, add the REFERENCE of the product";
+  String PRODUCT_REFERENCE = "Please, add the reference of the product";
   String OPTION_ONE = "1 - Add product to cart";
   String OPTION_TWO = "2 - Keep browsing products";
   String INVALID_OPTION = "Sorry, you have to choose one valid option";
