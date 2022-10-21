5f56f8cb521cc39929e7871ab33ce3e7e41a839b
julieta <julieta.martin@clarity.ai>
2022-10-21 09:38:01 +0200

    refactor: remove stock attribute from Product

--- app/src/main/java/gradle/java/domain/Product.java
+++ app/src/main/java/gradle/java/domain/Product.java
@@ -7,16 +7,14 @@ public class Product {
     public final String longDescription;
     public final Double price;
     public final String reference;
-    public final int stock;
 
-    public Product(String image, String description, String featuredAttribute, String longDescription, Double price, String reference, int stock){
+    public Product(String image, String description, String featuredAttribute, String longDescription, Double price, String reference){
         this.image = image;
         this.description = description;
         this.featuredAttribute = featuredAttribute;
         this.longDescription = longDescription;
         this.price = price;
         this.reference = reference;
-        this.stock = stock;
     }
 
     public String showImage(){
@@ -36,6 +34,4 @@ public class Product {
     }
 
     public String showReference(){ return reference; }
-
-    public int showStock() { return stock; }
 }
