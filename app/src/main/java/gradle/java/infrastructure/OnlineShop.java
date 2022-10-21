06f01e30e92f6fd0b50a94d84ac739b08a02c674
julieta <julieta.martin@clarity.ai>
2022-10-21 14:21:27 +0200

    refactor: add showProductByReference method into OnlineShop class

--- app/src/main/java/gradle/java/infrastructure/OnlineShop.java
+++ app/src/main/java/gradle/java/infrastructure/OnlineShop.java
@@ -2,17 +2,19 @@ package gradle.java.infrastructure;
 
 import gradle.java.domain.Product;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class OnlineShop {
-    private CatalogueFormatter catalogueFormater = new CatalogueFormatter();
     private final ProductWarehouse productWarehouse = new ProductWarehouse();
-    DecideNextSteps decideNextSteps = new DecideNextSteps();
-
+    Print print = new Print();
     public void showProducts() {
-
         ArrayList<Product> catalogue = productWarehouse.findAll();
-        String formattedCatalogue = catalogueFormater.format(catalogue);
-        System.out.println(formattedCatalogue);
-        decideNextSteps.decideWhatToDoNext();
+        print.printCatalogue(catalogue);
+    }
+    public void showProductByReference(){
+        Scanner myObj = new Scanner(System.in);
+        String object = myObj.nextLine();
+        Product product = productWarehouse.findProductByReference(object);
+        print.printByReference(product);
     }
 }
