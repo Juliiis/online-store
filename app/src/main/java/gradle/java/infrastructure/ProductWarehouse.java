02e5fa73c40cb47bc137366742972dc697d5b8f5
julieta <julieta.martin@clarity.ai>
2022-10-21 10:07:02 +0200

    refactor: move class decideShowASpecificProduct to DecideNextSteps

--- app/src/main/java/gradle/java/infrastructure/ProductWarehouse.java
+++ app/src/main/java/gradle/java/infrastructure/ProductWarehouse.java
@@ -3,10 +3,8 @@ package gradle.java.infrastructure;
 import gradle.java.domain.Product;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class ProductWarehouse {
-    GlobalVariables globalVariables = new GlobalVariables();
     AllProductsAttributes allProductsAttributes = new AllProductsAttributes();
     private final ArrayList<Product> products = new ArrayList<>(Arrays.asList(new Product(
                     allProductsAttributes.TV_IMAGE,
@@ -28,24 +26,5 @@ public class ProductWarehouse {
     }
 
     public int findStockByReference(String productReference){ return 0; }
-
-   /* public Product displayASpecificProduct(){
-
-        Scanner myObj = new Scanner(System.in);
-        String object = myObj.nextLine();
-
-        for(Product product : products){
-            if (object.equals("1")){
-                System.out.println(product.showImage());
-                System.out.println(product.showPrice());
-                System.out.println(product.showReference());
-                System.out.println(product.showDescription());
-                System.out.println(product.showFeaturedAttribute());
-                System.out.println("\n");
-            }
-            return product;
-        }
-        throw new RuntimeException(globalVariables.PRODUCT_DOES_NOT_EXIST);
-    }*/
 
 }
