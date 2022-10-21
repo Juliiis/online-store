627e9d4239e53ff2fb6c2480c7e9bb9e26fa8bec
julieta <julieta.martin@clarity.ai>
2022-10-21 09:51:25 +0200

    refactor: rename CatalogueFormatter

--- app/src/main/java/gradle/java/infrastructure/OnlineShop.java
+++ app/src/main/java/gradle/java/infrastructure/OnlineShop.java
@@ -1,12 +1,10 @@
 package gradle.java.infrastructure;
 
 import gradle.java.domain.Product;
-import gradle.java.infrastructure.CatalogueFormater;
-import gradle.java.infrastructure.ProductWarehouse;
 import java.util.ArrayList;
 
 public class OnlineShop {
-    private CatalogueFormater catalogueFormater = new CatalogueFormater();
+    private CatalogueFormatter catalogueFormater = new CatalogueFormatter();
     private final ProductWarehouse productWarehouse = new ProductWarehouse();
 
     public void showProducts() {
